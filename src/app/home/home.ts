1d89a3b18fa6f381078c6303e338afdab67e5d78
Bhakt Waghmare <bhaktipersonal98@gmail.com>
2026-03-11 11:59:08 +0530

    Initial commit

---
+++ src/app/home/home.ts
@@ -0,0 +1,63 @@
+import { Component } from '@angular/core'; // OnInit hata diya
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [RouterModule, CommonModule, ReactiveFormsModule],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home { // Implements OnInit hata diya
+  contactForm: FormGroup;
+  submitted = false;
+  error = false;
+
+  // FAQ Tab Logic
+  activeMainTab: string = 'Tab 1'; 
+  activeAccordionIndex: number = 0;
+
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      service: ['', Validators.required],
+      budget: ['', Validators.required],
+      message: ['', [Validators.required, Validators.minLength(10)]]
+    });
+  }
+
+  // Submit Logic
+  onSubmit() {
+    if (this.contactForm.valid) {
+      console.log('Form Data:', this.contactForm.value);
+      this.submitted = true;
+      this.error = false;
+      this.contactForm.reset();
+    } else {
+      this.error = true;
+    }
+  }
+
+  // FAQ List
+  faqList = [
+    { question: 'What services do you offer?', answer: 'Lorem ipsum...' },
+    { question: 'How do I get started?', answer: 'Lorem ipsum...' },
+    { question: 'Who do you work with?', answer: 'Lorem ipsum...' },
+    { question: 'Who is your ideal client?', answer: 'Lorem ipsum...' },
+    { question: 'What sets your agency apart?', answer: 'Lorem ipsum...' },
+    { question: 'How does your process work?', answer: 'Lorem ipsum...' },
+    { question: 'Which industries do you specialize in?', answer: 'Lorem ipsum...' }
+  ];
+
+  setMainTab(tab: string) {
+    this.activeMainTab = tab;
+    this.activeAccordionIndex = 0; 
+  }
+
+  toggleAccordion(index: number) {
+    this.activeAccordionIndex = this.activeAccordionIndex === index ? -1 : index;
+  }
+}
