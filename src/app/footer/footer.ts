42c9cbc615e33721be950cf899fc3f224e12c337
sunny-gil <sunnygill1706@gmail.com>
2026-03-14 12:20:26 +0530

    change footer code

--- src/app/footer/footer.ts
+++ src/app/footer/footer.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule,RouterModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './footer.html',
   styleUrls: ['./footer.css']
 })
-export class Footer {}
+export class Footer implements AfterViewInit {
+
+  @ViewChild('footerRef') footerRef!: ElementRef;
+
+  ngAfterViewInit() {
+
+    const footer = this.footerRef.nativeElement;
+
+    footer.addEventListener('mousemove', (e: MouseEvent) => {
+
+      const rect = footer.getBoundingClientRect();
+
+      const x = e.clientX - rect.left;
+      const y = e.clientY - rect.top;
+
+      footer.style.setProperty('--x', x + 'px');
+      footer.style.setProperty('--y', y + 'px');
+
+    });
+
+  }
+
+}
