204cd7bed755fc57035a1bfbafd3e040ef6fa91d
sunny-gil <sunnygill1706@gmail.com>
2026-03-14 19:26:02 +0530

    added hero section on aboute us clientand service page

--- src/app/client/client.ts
+++ src/app/client/client.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-client',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './client.html',
   styleUrl: './client.css',
 })
--- src/app/pages/service/service.ts
+++ src/app/pages/service/service.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-service',
-  imports: [CommonModule],
+  imports: [CommonModule,RouterLink],
   templateUrl: './service.html',
   styleUrls: ['./service.css'],
 })
